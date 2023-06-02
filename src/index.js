e417361be28ac4110b3759c7c4769b466c14da9f
pottsaf <aaronfpotts@gmail.com>
2023-06-01 20:45:44 -0600

    Create display function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,55 @@
-function task(title='Generic title', desc='Generic description of task', dueDate='Due yesterday', priority='Low', note='', checkList=[]) {
-    return {
-        title: title,
-        desc: desc,
-        dueDate: dueDate,
-        priority: priority,
-        note: note !== '' ? note : undefined,
-        checkList: checkList.length > 0 ? checkList : undefined,
-    }
+function task (title = 'Generic title', desc = 'Generic description of task', dueDate = 'Due yesterday', priority = 'Low', note = '', checkList = []) {
+  return {
+    title,
+    desc,
+    dueDate,
+    priority,
+    note: note !== '' ? note : undefined,
+    checkList: checkList.length > 0 ? checkList : undefined
+  }
 }
 
 const taskManager = (function () {
-    const taskArray = []
-    return {
-        addTask: function (title, desc, dueDate, priority, note, checkList) {
-            const newTask = task(title, desc, dueDate, priority, note, checkList);
-            taskArray.push(newTask)
-        },
-        deleteTask: function (index) {
-            if (index >= 0 && index < taskArray.length) {
-                taskArray.splice(index, 1)
-            }
-        },
-        editTask: function (index, updatedTask) {
-            if (index >= 0 && index < taskArray.length) {
-                taskArray[index] = updatedTask
-        }
+  const taskArray = []
+  return {
+    addTask: function (title, desc, dueDate, priority, note, checkList) {
+      const newTask = task(title, desc, dueDate, priority, note, checkList)
+      taskArray.push(newTask)
     },
-        getAllTasks: function () {
-            return taskArray
-        }
-}
+    deleteTask: function (index) {
+      if (index >= 0 && index < taskArray.length) {
+        taskArray.splice(index, 1)
+      }
+    },
+    editTask: function (index, updatedTask) {
+      if (index >= 0 && index < taskArray.length) {
+        taskArray[index] = updatedTask
+      }
+    },
+    getAllTasks: function () {
+      return taskArray
+    },
+    showTask: function (n) {
+      return taskArray[n]
+    }
+  }
 })()
 
+const displayTasks = function () {
+  const currentTasks = taskManager.getAllTasks()
+  for (let i = 0; i < currentTasks.length; i++) {
+    const deployTaskTitle = document.createElement('div')
+    const deployTaskDesc = document.createElement('div')
+
+    deployTaskTitle.innerHTML = taskManager.showTask(i).title
+    deployTaskDesc.innerHTML = taskManager.showTask(i).desc
+
+    document.body.appendChild(deployTaskTitle)
+    document.body.appendChild(deployTaskDesc)
+  }
+}
+
 taskManager.addTask('First', 'Create a task', 'Tomorrow', 'Low')
+taskManager.addTask('Second', 'Display the tasks', 'Tonight', 'Low')
+
+displayTasks()
